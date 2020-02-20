b622bb9bcee3b54c90190ede8a4cbc84191ee6b1
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-02-20 14:59:38 -0500

    Initial Commit

---
+++ LocalNotificationsLab/CreatingVC.swift
@@ -0,0 +1,22 @@
+//
+//  CreatingVC.swift
+//  LocalNotificationsLab
+//
+//  Created by Christian Hurtado on 2/20/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class CreatingVC: UIViewController {
+    
+    @IBOutlet weak var pickerView: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
---
+++ LocalNotificationsLab/NotificationsVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalNotificationsLab
+//
+//  Created by Christian Hurtado on 2/20/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class NotificationsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var notifications = [""]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
